3b71fedf0f08e166d0b879c0652ff6279c6b9521
Auto Logon <autologon@CSED.LAB>
2024-08-22 17:59:58 -0300

    Quinta feira

--- WorkShop/Assets/Scripts/playcontroler.cs
+++ WorkShop/Assets/Scripts/playcontroler.cs
@@ -21,18 +21,47 @@ public class playcontroler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        inputX = Input.GetAxisRaw("Horizontal");
-        inputJump = Input.GetKeyDown(KeyCode.Space);
-
-        if(inputJump == true && inGround == true)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, forceJump);
-        }
+        Inputs();
+        JumpLogic();
 
     }
     private void FixedUpdate()
     {
+       MoveLogic();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            inGround = true;
+        }
+    }
+     
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            inGround = false;
+        }
+    }
+
+    public void Inputs()
+    {
+         inputX = Input.GetAxisRaw("Horizontal");
+         inputJump = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    public void JumpLogic()
+    {
+         if(inputJump == true && inGround == true)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, forceJump);
+        }
+    }
+
+    public void MoveLogic()
+    {
         rb.velocity = new Vector2(inputX * speed, rb.velocity.y);
     }
 }
-
