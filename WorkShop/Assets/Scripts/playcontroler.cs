a3e72fe6053b6bfbb01dbf5832b213ab2510664d
Auto Logon <autologon@CSED.LAB>
2024-08-23 17:06:38 -0300

    wow

--- WorkShop/Assets/Scripts/playcontroler.cs
+++ WorkShop/Assets/Scripts/playcontroler.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playcontroler : MonoBehaviour
+public class Playcontroler : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float inputX;
     public bool inputJump;
 
-    public float speed;
+
+    public float speed;    
     public float forceJump;
 
     public bool inGround;
+
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,11 +27,13 @@ public class playcontroler : MonoBehaviour
     {
         Inputs();
         JumpLogic();
-
+        Animations();
+        Flip();
     }
+
     private void FixedUpdate()
     {
-       MoveLogic();
+	    MoveLogic();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -37,7 +43,7 @@ public class playcontroler : MonoBehaviour
             inGround = true;
         }
     }
-     
+
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
@@ -46,22 +52,42 @@ public class playcontroler : MonoBehaviour
         }
     }
 
+    public void Animations()
+    {
+        anim.SetFloat("Horizontal", rb.velocity.x);
+    }
+
     public void Inputs()
     {
-         inputX = Input.GetAxisRaw("Horizontal");
-         inputJump = Input.GetKeyDown(KeyCode.Space);
+        inputX = Input.GetAxisRaw("Horizontal");
+	    inputJump = Input.GetKeyDown(KeyCode.Space);
     }
 
     public void JumpLogic()
     {
-         if(inputJump == true && inGround == true)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, forceJump);
-        }
+        if(inputJump == true && inGround == true)
+	    {
+	    	rb.velocity = new Vector2(rb.velocity.x, forceJump);
+	    }
     }
 
     public void MoveLogic()
     {
         rb.velocity = new Vector2(inputX * speed, rb.velocity.y);
     }
+    
+    public void Flip()
+    {
+        if (inputX < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else if(inputX > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+    }
+
 }
+
+
